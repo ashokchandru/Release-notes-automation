5945ff16d4690321ca4986430be31362b4ad53e5
Ashok Kumar <ashokchandru@gmail.com>
2025-11-17 20:45:46 +0530

    Upload mapping code python file

---
+++ Mapping_code.py
@@ -0,0 +1,7 @@
+SECTION_MAP = {
+    "New Features": "new_features",
+    "Known Issues": "known_issues",
+    "Improvements": "improvements",
+    "Bug Fixes": "bug_fixes",
+    "Enhancements": "enhancements",
+}
